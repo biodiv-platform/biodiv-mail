16ed5e1e16744a5b201f527823afcdd41a3fba51
prakhar-s <prakhardevsingh4@gmail.com>
2025-11-25 14:24:26 +0530

    Revert "version inconsistancies debug-iter4"
    This reverts commit 57c980b17f8e96dfdacef1bb638ffd4dda6cab79.

--- @ci/build-and-deploy.sh
+++ @ci/build-and-deploy.sh
@@ -9,9 +9,4 @@ chmod +x @ci/maven-toolbox
 ./@ci/maven-toolbox configure-pre-sdk
 mvn clean install
 ./@ci/maven-toolbox configure-sdk
-
-# Fix CVE-2022-42889: Inject commons-text 1.13.0 and upgrade javadoc plugin after maven-toolbox
-sed -i 's|<version>3.3.2</version>|<version>3.8.0</version>|g' target/sdk/pom.xml
-sed -i '/<\/properties>/a\\n    <dependencyManagement>\n        <dependencies>\n            <dependency>\n                <groupId>org.apache.commons</groupId>\n                <artifactId>commons-text</artifactId>\n                <version>1.13.0</version>\n            </dependency>\n        </dependencies>\n    </dependencyManagement>' target/sdk/pom.xml
-
 mvn -f target/sdk/pom.xml deploy
